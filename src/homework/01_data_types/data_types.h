4e81fd20fb61b373707a1e19f1b57dabe6481210
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-09-12 16:25:17 +0000

    got rid of example

--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -1,9 +1,3 @@
-
-﻿// Multiplies two integers and returns the result.
+﻿
+// Multiplies two integers and returns the result.
 int multiply_numbers(int num1, int num2);
-
-﻿//example
-int add_numbers(int num1, int num2);
-
-
-
