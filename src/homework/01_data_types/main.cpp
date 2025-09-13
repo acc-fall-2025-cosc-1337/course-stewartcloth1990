d34c1d59325344937fa8f289412a55fb0ea8173c
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-09-13 03:15:50 +0000

    cleaned up code for data types and used it in main function block

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -4,11 +4,16 @@
 
 
 //write namespace using statement for cout
-
+using std::cout;
 
 int main()
 {
-	
+
+	int result = multiply_numbers(7, 7);
+	cout << "Result: " << result << "\n";
+
+	result = multiply_numbers(5, 5);
+	cout << "Result: " << result << "\n";
 
 	return 0;
 }
--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -11,3 +11,11 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Verify multiply_numbers function") 
+{
+	REQUIRE(multiply_numbers(2, 3) == 6);
+	REQUIRE(multiply_numbers(0, 10) == 0);
+	REQUIRE(multiply_numbers(-4, 5) == -20);
+	REQUIRE(multiply_numbers(7, -2) == -14);
+	REQUIRE(multiply_numbers(-3, -3) == 9);
+}
