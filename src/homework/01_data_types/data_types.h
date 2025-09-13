a6ceea9e7c34fa1e70de8a23fd38db3e61c025ab
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-09-13 02:41:34 +0000

    reset files to fix some issues

--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -1,9 +1,5 @@
-
-﻿// Multiplies two integers and returns the result.
-int multiply_numbers(int num1, int num2);
-
 ﻿//example
 int add_numbers(int num1, int num2);
 
-
-
+//write function prototype here
+int multiply_numbers(int num1, int num2);
