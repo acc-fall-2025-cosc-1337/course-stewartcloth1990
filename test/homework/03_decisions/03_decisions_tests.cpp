58ef5a243005d260555ad4d03bbfb5b83933f27c
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-09-23 02:43:01 +0000

    added header, test cases, functions, and main function

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -1,46 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.h"
+#include "decisions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
-
-TEST_CASE("AND Truth Table", "[and]") {
-	REQUIRE((false && false) == false);
-	REQUIRE((false && true) == false);
-	REQUIRE((true && false) == false);
-	REQUIRE((true && true) == true);
-}
-
-TEST_CASE("OR Truth Table", "[or]") {
-	REQUIRE((false || false) == false);
-	REQUIRE((false || true) == true);
-	REQUIRE((true || false) == true);
-	REQUIRE((true || true) == true);
-}
-
-TEST_CASE("NOT Truth Table", "[not]") {
-	REQUIRE((!false) == true);
-	REQUIRE((!true) == false);
-}
-
-TEST_CASE("is_vowel function", "[is_vowel]") {
-	REQUIRE(is_vowel('a') == true);
-	REQUIRE(is_vowel('e') == true);
-	REQUIRE(is_vowel('i') == true);
-	REQUIRE(is_vowel('o') == true);
-	REQUIRE(is_vowel('u') == true);
-	REQUIRE(is_vowel('b') == false);
-	REQUIRE(is_vowel('y') == false);
-}
-
-TEST_CASE("is_consonant function", "[is_consonant]") {
-	REQUIRE(is_consonant('a') == false);
-	REQUIRE(is_consonant('e') == false);
-	REQUIRE(is_consonant('i') == false);
-	REQUIRE(is_consonant('o') == false);
-	REQUIRE(is_consonant('u') == false);
-	REQUIRE(is_consonant('b') == true);
-	REQUIRE(is_consonant('y') == true);
-}
+TEST_CASE("get_letter_grade_using_if returns correct letter grade for valid scores") {
+	REQUIRE(get_letter_grade_using_if(95) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(50) == "F");
+	}
