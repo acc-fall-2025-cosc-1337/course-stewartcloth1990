8acdca2109613b17f916a1a9d83aeeed9c42a556
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-10-04 23:47:55 +0000

    add main code block, finished hw04

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -4,17 +4,20 @@
 //add function(s) code here
 
 // Returns the factorial of a given number 'num'.
-int get_factorial(int num){
+int get_factorial(int num)
+{
     int factorial = 1;
     
-    for (int i = 1; i <= num; ++i){
+    for (int i = 1; i <= num; ++i)
+    {
         factorial *= i;
     }
     return factorial;
 }
 
 // Returns the sum of all odd numbers from 1 up to and including 'num'.
-int sum_odd_numbers(int num){
+int sum_odd_numbers(int num)
+{
     int sum = 0;
     int i = 1;
 
