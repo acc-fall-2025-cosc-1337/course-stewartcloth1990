c74103efa010d5514f6f0da975654a7f5097e5d3
stewartcloth1990 <stewart.cloth@g.austincc.edu>
2025-11-13 22:33:47 +0000

    add header guards, created manager class in header

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,24 @@
 //h
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+class TicTacToeManager
+{
+public:
+    void save_game(TicTacToe b);
+    void get_winner_total(int& o, int& w, int&t);
+
+
+private:
+    std::vector<TicTacToe> games;
+    int o_wins = 0;
+    int x_wins = 0;
+    int ties = 0;
+};
+
+#endif
